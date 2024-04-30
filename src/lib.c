53ebf69a8ea9fe8ca0150d21d8b7a5daeb709c84
kakashi <trishiragolder69@outlook.com>
2024-05-01 02:56:11 +0530

    fixed vec again

--- src/lib.c
+++ src/lib.c
@@ -1,28 +1,25 @@
 #include "prelude.h"
 #include "alloc.h"
+#include <string.h>
+#include <assert.h>
 
 #define DEFAULT_SIZE 16
+#define as_vec(ptr) ((Vec*)ptr)-1
+#define non_null2(ptr1,ptr2) assert(ptr1!=NULL);assert(ptr2!=NULL)
 
-void* new_vec(usize BYTES_PER_ELEMENT) {
+typedef Vec* Self;
+
+Vec new_vec(usize BYTES_PER_ELEMENT) {
   return new_vec_with_capacity(DEFAULT_SIZE,BYTES_PER_ELEMENT);
 }
 
-void* new_vec_with_capacity(usize capacity,usize BYTES_PER_ELEMENT) {
-  Vec* self=alloc(sizeof(Vec)+(capacity*BYTES_PER_ELEMENT));
+Vec new_vec_with_capacity(usize capacity,usize BYTES_PER_ELEMENT) {
+  Vec self;
 
-  self->BYTES_PER_ELEMENT=BYTES_PER_ELEMENT;
-  self->capacity=capacity;
-  self->len=0;
+  self.BYTES_PER_ELEMENT=BYTES_PER_ELEMENT;
+  self.capacity=capacity;
+  self.len=0;
+  self.ptr=alloc(capacity*BYTES_PER_ELEMENT);
 
-  return self+1;
+  return self;
 }
-
-
-
-
-
-
-
-
-
-
--- src/lib.h
+++ src/lib.h
@@ -1,19 +1,22 @@
 #ifndef VEC_LIB_H
 #define VEC_LIB_H
 
-#include "prelude.h"
 
 
 typedef __SIZE_TYPE__ usize;
 
+/**
+ * `Vec` shands for `vector`
+ */
 typedef struct Vec {
   usize BYTES_PER_ELEMENT;
   usize len;
   usize capacity;
+  void* ptr;
 } Vec;
 
 
-void* new_vec(usize BYTES_PER_ELEMENT);
-void* new_vec_with_capacity(usize capacity,usize BYTES_PER_ELEMENT);
+Vec new_vec(usize BYTES_PER_ELEMENT);
+Vec new_vec_with_capacity(usize capacity,usize BYTES_PER_ELEMENT);
 
 #endif
